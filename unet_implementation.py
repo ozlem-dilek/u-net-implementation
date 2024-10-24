fb054540d5ec14a36ac396deb38c62dda81cab89
Ozlem Dilek Acar <acarozlemdilek@gmail.com>
2024-10-24 18:02:51 +0300

    Down class

--- unet_implementation.py
+++ unet_implementation.py
@@ -8,8 +8,7 @@ class DoubleConv(nn.Module):
         if mid_channels == None:
             mid_channels = out_channels
 
-        def doubleConv(self, in_channels, out_channels, mid_channels):
-          return nn.Sequential(
+        self.doubleConv = nn.Sequential(
               nn.Conv2d(in_channels = in_channels, out_channels = mid_channels, kernel_size = 3, padding = 1, stride = 1),
               nn.BatchNorm2d(mid_channels),
               nn.ReLU(inplace = True),
@@ -18,6 +17,18 @@ class DoubleConv(nn.Module):
               nn.ReLU(inplace = True)
           )
 
-        def forward(self, x):
+    def forward(self, x):
           return self.doubleConv(x)
+
+class Down(nn.Module):
+  def __init__(self, in_channels, out_channels, mid_channels = None):
+    super().__init__()
+
+    self.doubleConv = DoubleConv(in_channels = in_channels, out_channels = out_channels, mid_channels = mid_channels)
+    self.maxPool = nn.MaxPool2d(kernel_size = 2, stride = 2)
+
+  def forward(self, x):
+    x = self.doubleConv(x)
+    
+    return self.maxPool(x)
 
