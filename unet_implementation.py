d5aefbfdbfa68c0916e2256b12eb811f96b5bcd6
Ozlem Dilek Acar <acarozlemdilek@gmail.com>
2024-10-24 18:30:47 +0300

    U-Net class

--- unet_implementation.py
+++ unet_implementation.py
@@ -43,3 +43,33 @@ class Up(nn.Module):
     x = self.deConv(x)
     x = torch.cat((x, x_skip), dim = 1)
     return self.doubleConv(x)
+  
+
+class UNet(nn.Module):
+  def __init__(self, in_channels, num_class):
+    super().__init__()
+
+    self.inc = DoubleConv(in_channels, 64)
+    self.down1 = Down(64, 128)
+    self.down2 = Down(128, 256)
+    self.down3 = Down(256, 512)
+    self.down4 = Down(512, 1024)
+
+    self.up1 = Up(1024, 512)
+    self.up2 = Up(512, 256)
+    self.up3 = Up(256, 128)
+    self.up4 = Up(128, 64)
+
+  def forward(self, x):
+    inc = self.inc(x)
+    down1 = self.down1(inc)
+    down2 = self.down2(down1)
+    down3 = self.down3(down2)
+    down4 = self.down4(down3)
+
+    x = self.up1(down4, down3)
+    x = self.up2(x, down2)
+    x = self.up3(x, down1)
+    x = self.up4(x, inc)
+
+    return x
